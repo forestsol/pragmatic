7a6cd2f0f5bdf0862f9bd6a386bcc0971861dbd4
forestsol <forestsol1214@gmail.com>
2024-09-03 19:47:10 +0900

    Initial commit

---
+++ accountapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from accountapp.views import hello_world
+
+appname = "accountapp"
+
+"127.0.0.1:8000/account/hello_world/"
+"-> accountapp:hello_world"  #hello_world는 뷰 명을 적는게 아니라, urlpatterns에서 지정해 둔 name임.
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world')
+]
