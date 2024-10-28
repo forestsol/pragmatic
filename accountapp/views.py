d17f9ca33f8b3f8d1331c6d11ca025b081ad34d0
forestsol <forestsol1214@gmail.com>
2024-10-28 20:55:08 +0900

    Django course 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -16,9 +17,11 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        # hello_world로 리다이렉트. post가 아닐 땐 get이므로 get으로 리다이렉트 될 것.
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
         # new_hello_world는 객체임.
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET Method'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
