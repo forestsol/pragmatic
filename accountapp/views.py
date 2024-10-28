a9bb5a54668f3d42a8d1dad3f5b647bca668fb0b
forestsol <forestsol1214@gmail.com>
2024-10-28 18:14:10 +0900

    django course 17 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import HelloWorld
+
+
 # Create your views here.
 
+
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        # new_hello_world는 객체임.
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET Method'})
 
 
