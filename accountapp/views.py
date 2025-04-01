35dd3c1958d849e25ae16469f7b3cdd558ddb104
forestsol <51287968+forestsol@users.noreply.github.com>
2025-04-01 19:13:59 +0900

    django course 28 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,36 +1,38 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
 # Create your views here.
 
+has_ownered = [login_required, account_ownership_required]
 
+@login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
+    if request.method == "POST":
 
-            temp = request.POST.get('hello_world_input')
+        temp = request.POST.get('hello_world_input')
 
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-            # hello_world로 리다이렉트. post가 아닐 땐 get이므로 get으로 리다이렉트 될 것.
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-            # new_hello_world는 객체임.
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
+        # hello_world로 리다이렉트. post가 아닐 땐 get이므로 get으로 리다이렉트 될 것.
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        # new_hello_world는 객체임.
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
 
 class AccountCreateView(CreateView):
     model = User
@@ -46,6 +48,8 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
+@method_decorator(has_ownered, 'get')
+@method_decorator(has_ownered, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -53,32 +57,11 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:  # self.get_object는, url에서 받은 pk로 검색한 유저객체 self.request.user는 지금 요청을 넣고 있는 유저객체
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
+@method_decorator(has_ownered, 'get')
+@method_decorator(has_ownered, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
-
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().get(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
