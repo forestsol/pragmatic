7562baad72331038bbb83c7e9ba0ebf68526a97e
forestsol <51287968+forestsol@users.noreply.github.com>
2025-04-13 22:48:44 +0900

    2025-04-13 django study
    2025-04-13 django study

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
 
 # Create your views here.
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = "profile"
+    form_class = ProfileCreationForm
